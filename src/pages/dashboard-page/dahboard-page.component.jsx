1fd56c289252e72dc6c9a60f8a1fd8049019fa54
Francis Nduamaka <francis.nduamaka@gmail.com>
2020-01-24 07:42:13 +0100

    create a start template for the dashboard page

--- src/pages/dashboard-page/dahboard-page.component.jsx
+++ src/pages/dashboard-page/dahboard-page.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './dashboard-page.styles.scss';
 
 export default function DashBoardPage() {
   return (
-    <div>
-      DashBoardPage
+    <div class="grid-container">
+      <div class="item1">Header</div>
+      <div class="item2">Menu</div>
+      <div class="item3"> Main</div>
+      <div class="item4">Right</div>
+      <div class="item5">card 1</div>
+      <div class="item6">card 2</div>
     </div>
   );
 }
